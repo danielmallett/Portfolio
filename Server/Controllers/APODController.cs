c259e5161f262fa29b554d92c7d368795c5c0fb0
daniel <dmallett@vols.utk.edu>
2023-08-20 22:09:59 -0400

    initial commit

---
+++ Server/Controllers/APODController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Portfolio.Server.Controllers{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APODController : ControllerBase{
+
+        [HttpGet]
+        public async Task<ActionResult> GetRequestAsync(){
+            string ret = "fuck";
+
+            
+            string keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
+            string kvUri = "https://" + keyVaultName + ".vault.azure.net";
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+            var secret = client.GetSecret("APODKEY");
+            string key = secret.Value.Value.ToString();
+
+            using (HttpClient httpClient = new HttpClient()){
+                ret = await httpClient.GetStringAsync("https://api.nasa.gov/planetary/apod?api_key=" + key);
+            }
+            
+            return Ok(ret);
+        }
+
+
+    }
+}
---
+++ Shared/APODModel.cs
@@ -0,0 +1,17 @@
+public class APODModel{
+    public string Copyright {get; set;}
+
+       public string Date {get; set;}
+
+       public string Explanation {get; set;}
+
+       public string HDURL {get; set;}
+
+       public string MediaType {get; set;}
+
+       public string ServiceVersion {get; set;}
+
+       public string Title {get; set;}
+
+       public string URL {get; set;}
+}
